ee6b83e991407faeb2b274057deb7a9c39af7fac
Priyanshu-code-hustler <priyanshuraj979837@gmail.com>
2025-07-18 00:58:42 +0530

    updated backend files

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -82,4 +82,4 @@ userSchema.methods.generateRefreshToken = function(){
     }
    )
 }
-export const User = mongoose.Schema.model("User",userSchema)
+export const User = mongoose.model("User",userSchema)
